ddb3fe1cce5d06f53799894697455990c6add2ed
mukhammadaminovBobur <virtualcoder20@gmail.com>
2024-11-27 21:55:16 +0100

    16. create a MySQL Database 17. PDO first steps, connecting to db from php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
 require "functions.php";
-require "router.php";
+//require "router.php";
+
+//connect to the database
+$dns = "mysql:host=localhost;port=3306;dbname=phpForBeginners;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dns);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+
+}
