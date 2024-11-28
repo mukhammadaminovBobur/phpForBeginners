2ea42865e3c051be8f5e6f48c2507f3e96085890
mukhammadaminovBobur <virtualcoder20@gmail.com>
2024-11-28 21:39:09 +0100

    20. SQL Injection Vulnerabilities Explained

--- index.php
+++ index.php
@@ -9,10 +9,12 @@ require "Database.php";
 $config = require "config.php";
 
 $db = new Database($config['database']);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
-foreach ($posts as $post) {
-    echo "<li>{$post['title']}</li>";
+$id = $_GET['id'];
+//do not put the $id directly into the query from the user input
+$query = "SELECT * FROM posts where id = :id";
 
-}
+$posts = $db->query($query, ["id" => $id])->fetch();
+
+dd($posts);
 
