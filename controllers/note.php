d19d5a908dcbb91a85be79c1c9f8023202a4e518
mukhammadaminovBobur <virtualcoder20@gmail.com>
2024-11-28 23:37:53 +0100

    23. Introduction to Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,8 +4,19 @@ $config = require "config.php";
 $db = new Database($config['database']);
 
 $heading = "Note";
+$user_id = 4;
 
-$note = $db->query("SELECT * FROM notes where id = :id", ['id' => $_GET['id']])->fetch();
+$note = $db->query("SELECT * FROM notes where id = :id", [
+    'id' => $_GET['id']
+])->fetch();
 
+if (!$note) {
+    abort(Response::NOT_FOUND);
+}
+
+
+if ($note['user_id'] != $user_id) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
